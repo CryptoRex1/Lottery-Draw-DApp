4bcd79170bf33d2b687f03583ff9343c74ce99fa
CryptoRex1 <107986220+CryptoRex1@users.noreply.github.com>
2023-08-22 21:13:36 +0200

    Update AdminControls.tsx

--- components/AdminControls.tsx
+++ components/AdminControls.tsx
@@ -1,54 +1,29 @@
-import { ArrowPathIcon, ArrowUturnDownIcon, CurrencyDollarIcon, StarIcon } from '@heroicons/react/24/solid';
+import { StarIcon, CurrencyDollarIcon, ArrowPathIcon, ArrowUturnDownIcon } from '@heroicons/react/24/solid';
 import React from 'react';
-import { useContract, useMetamask, useDisconnect, useAddress, useContractRead, useContractWrite } from '@thirdweb-dev/react';
+import { useContractRead, useContractWrite } from '@thirdweb-dev/react';
 import { ethers } from 'ethers';
 import { currency } from '../constants';
 import toast from 'react-hot-toast';
 
-function AdminControls() {
+function AdminControls({ contract }) {
 
-    const {contract, isLoading} = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
+    const { data: totalCommission } = useContractRead(contract, "operatorTotalCommission");
 
-    const {data: totalCommission} = useContractRead(contract, "operatorTotalCommission");
-
-    const {mutateAsync: DrawWinnerTicket} = useContractWrite(contract, "DrawWinnerTicket");
+    const { mutateAsync: drawWinner } = useContractWrite(contract, "DrawWinnerTicket");
     
-    const { mutateAsync: RefundAll } = useContractWrite(contract, "RefundAll");
+    const { mutateAsync: refundAll } = useContractWrite(contract, "RefundAll");
     
     const { mutateAsync: restartDraw } = useContractWrite(contract, "restartDraw");
     
-    const { mutateAsync: WithdrawCommission } = useContractWrite(contract, "WithdrawCommission");
+    const { mutateAsync: withdrawCommission } = useContractWrite(contract, "WithdrawCommission");
 
-    const drawWinner = async () => {
-
-        const notification = toast.loading("Picking a Lucky Winner...");
-
-        try {
-            const data = await DrawWinnerTicket([{}]);
-
-            toast.success("Winner Picked!", {
-                id: notification,
-            });
-
-            console.info("Contract call success", data);
-        }
-        catch (err) {
-            toast.error("Whoops something went wrong!", {
-                id: notification,
-            });
-
-            console.error("Contract call failed", err);
-        }
-    }
-
-
-    const onWithdrawCommission = async () => {
-        const notification = toast.loading("Withdrawing the commission...");
+    const handleContractCall = async (mutateFunction, message) => {
+        const notification = toast.loading(message);
 
         try {
-            const data = await WithdrawCommission([{}]);
+            const data = await mutateFunction([{}]);
 
-            toast.success("Commission has been withdrawn successfully!", {
+            toast.success("Action successful!", {
                 id: notification,
             });
 
@@ -63,53 +38,6 @@ function AdminControls() {
         }
     }
 
-
-    const onRestartDraw = async () => {
-        const notification = toast.loading("Restarting the draw...");
-
-        try {
-            const data = await restartDraw([{}]);
-
-            toast.success("Draw restarted successfully!", {
-                id: notification,
-            });
-
-            console.info("Contract call success", data);
-        }
-        catch (err) {
-            toast.error("Whoops, something went wrong!", {
-                id: notification,
-            });
-
-            console.error("Contract call failed", err);
-        }
-    }
-
-
-    const onRefundAll = async () => {
-        const notification = toast.loading("Refunding all...");
-
-        try {
-            const data = await RefundAll([{}]);
-
-            toast.success("All refunded successfully!", {
-                id: notification,
-            });
-
-            console.info("Contract call success", data);
-        }
-        catch (err) {
-            toast.error("Whoops, something went wrong!", {
-                id: notification,
-            });
-
-            console.error("Contract call failed", err);
-        }
-    };
-
-
-
-    
     return (
         <div className='text-white text-center px-5 py-3 rounded-md border-emerald-300/20 border'>
             <h2 className='font-bold'>Admin Controls</h2>
@@ -118,25 +46,25 @@ function AdminControls() {
                 {" "}{currency}</p>
             <div className='flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2'>
                 <button
-                    onClick={drawWinner}
+                    onClick={() => handleContractCall(drawWinner, "Picking a Lucky Winner...")}
                     className='admin-button'>
                     <StarIcon className='h-6 mx-auto mb-2' />
                     Draw Winner
                 </button>
                 <button
-                    onClick={onWithdrawCommission}
+                    onClick={() => handleContractCall(withdrawCommission, "Withdrawing the commission...")}
                     className='admin-button'>
                     <CurrencyDollarIcon className='h-6 mx-auto mb-2' />
                     Withdraw Commission
                 </button>
                 <button
-                    onClick={onRestartDraw}
+                    onClick={() => handleContractCall(restartDraw, "Restarting the draw...")}
                     className='admin-button'>
                     <ArrowPathIcon className='h-6 mx-auto mb-2' />
                     Restart Draw
                 </button>
                 <button
-                    onClick={onRefundAll}
+                    onClick={() => handleContractCall(refundAll, "Refunding all...")}
                     className='admin-button'>
                     <ArrowUturnDownIcon className='h-6 mx-auto mb-2' />
                     Refund All
@@ -146,4 +74,4 @@ function AdminControls() {
     )
 }
 
-export default AdminControls
+export default AdminControls;
