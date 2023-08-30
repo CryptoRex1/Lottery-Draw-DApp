b72f00d5dd97c315e4373aed94d467363dc38cd0
CryptoRex1 <107986220+CryptoRex1@users.noreply.github.com>
2023-08-30 12:03:31 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,18 @@ import { AppProps } from 'next/app';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
+// If used on the FRONTEND pass your 'clientId'
+const sdk = new ThirdwebSDK("polygon", {
+  clientId: "YOUR_CLIENT_ID",
+
+
+const contract = await sdk.getContract("0xCb7aB8F3Cf115c08F3741f9Eff409f8E3529dc7C");
+
     // Add any global initialization or setup here
   }, []);
 
